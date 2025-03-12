e0063fa2a3507f40bdaf09f0022bb6c99bedd010
Kavirubc <hapuarachchikaviru@gmail.com>
2025-03-12 20:35:44 +0530

    first commit

---
+++ src/ai_news/crew.py
@@ -0,0 +1,97 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, FileWriterTool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# If you want to run a snippet of code before or after the crew starts, 
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class AiNews():
+	"""AiNews crew"""
+
+	# Learn more about YAML configuration files here:
+	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	ollama_llm = LLM(
+		model="ollama/deepseek-r1:8b ",
+		base_url="http://localhost:11434",
+	)
+
+	# If you would like to add tools to your agents, you can learn more about it here:
+	# https://docs.crewai.com/concepts/agents#agent-tools
+	@agent
+	def retrieve_news(self) -> Agent:
+		return Agent(
+			config=self.agents_config['retrieve_news'],
+			tools=[SerperDevTool()],
+			verbose=True,
+		)
+
+	@agent
+	def website_scraper(self) -> Agent:
+		return Agent(
+			config=self.agents_config['website_scraper'],
+			tools=[ScrapeWebsiteTool()],
+			verbose=True,
+			# llm=self.ollama_llm
+		)
+	
+	@agent
+	def ai_news_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['ai_news_writer'],
+			tools=[],
+			verbose=True,
+			# llm=self.ollama_llm
+		)
+	
+	@agent
+	def file_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['file_writer'],
+			tools=[FileWriterTool()],
+			verbose=True,
+			# llm=self.ollama_llm
+		)
+
+	@task
+	def retrieve_news_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['retrieve_news_task'],
+		)
+
+	@task
+	def website_scrape_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['website_scrape_task'],
+		)
+	
+	@task
+	def ai_news_write_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['ai_news_write_task'],
+		)
+	
+	@task
+	def file_write_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['file_write_task'],
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the AiNews crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
